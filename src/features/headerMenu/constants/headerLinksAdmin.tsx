800d76cb9eabbe9fd150710f84c5d12e25b0431e
first <Katya_Klebleeva@mail.ru>
2024-12-19 17:03:51 +0300

    удалено ненужное

--- src/features/headerMenu/constants/headerLinksAdmin.tsx
+++ src/features/headerMenu/constants/headerLinksAdmin.tsx
@@ -2,10 +2,6 @@ import { ManagerEventsIcon, ProfileAdminIcon } from '@/shared/icons';
 import { THeaderLinks } from '@/features/headerMenu/types';
 
 export const headerLinksAdmin: THeaderLinks[] = [
-	// {
-	// 	link: '/events',
-	// 	icon: <EventsIcon />,
-	// },
 	{
 		link: '/admin',
 		icon: <ManagerEventsIcon />,
